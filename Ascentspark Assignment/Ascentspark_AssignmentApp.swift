b18a0004bea81e2eae21e0197556dfd06ecb75f2
Aniruddha <aniruddha200baidya@protonmail.com>
2021-09-09 15:09:48 +0100

    Initial Commit

---
+++ Ascentspark Assignment/Ascentspark_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ascentspark_AssignmentApp.swift
+//  Ascentspark Assignment
+//
+//  Created by administrator on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Ascentspark_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
